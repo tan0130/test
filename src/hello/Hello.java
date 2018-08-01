a45dacb789b87d698d6273abdfe7cfbd88d3ac9d
tan.jinming <1311230692@qq.com>
2018-08-01 13:36:34 +0800

    git test!!!

---
+++ src/hello/Hello.java
@@ -0,0 +1,12 @@
+package hello;
+/**
+ * create by 1311230692@qq.com on 2018年8月1日 下午1:34:30
+ **/
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World !!!");
+
+	}
+
+}
